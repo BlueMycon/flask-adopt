ad94cb3ba06a7a907d33932cd86c84de27815db1
Kenny Burgher <kenny.burgher@gmail.com>
2023-04-28 12:32:03 -0700

    models, add form started, homepage

--- forms.py
+++ forms.py
@@ -1,1 +1,19 @@
-"""Forms for adopt app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, FloatField
+from wtforms.validators import Optional, URL, InputRequired
+
+class AddPetForm(FlaskForm):
+    """Forms for adopt app."""
+    name = StringField(
+        "Pet Name",
+        validators=[InputRequired()])
+    species = SelectField(
+        "Species",
+        choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')])
+    photo_url = StringField(
+        "Photo URL",
+        validators=[Optional(), URL()])
+    age = SelectField(
+        "Age",
+        choices=[('baby', 'Baby'), ('young', 'Young'), ('adult', 'Adult'), ('senior', 'Senior')])
+    notes = StringField("Notes")
--- models.py
+++ models.py
@@ -5,6 +5,47 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+class Pet(db.Model):
+    __tablename__ = "pets"
+
+    # id: auto-incrementing integer
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+    # name: text, required
+    name = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+    # species: text, required
+    species = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+    # photo_url: text, required (we’ll make it '' if they don’t give us one)
+    photo_url = db.Column(
+        db.Text,
+        nullable=False,
+        default='',
+    )
+    # age: text, (baby, young, adult, senior) required
+    age = db.Column(
+        db.String(20),
+        nullable=False,
+    )
+    # notes: text, optional
+    notes = db.Column(
+        db.Text,
+    )
+    # available: true/false, required, should default to available
+    available = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=True,
+    )
+
 def connect_db(app):
     """Connect this database to provided Flask app.
 
